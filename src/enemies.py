2b82391698352a5ea4ff9fdc07f524483ac5a89e
aidenbilling <aidenbilling2007@gmail.com>
2025-04-01 14:31:22 +0800

    Rewrote enemies file to fit with the new base class.

--- src/enemies.py
+++ src/enemies.py
@@ -1,72 +1,23 @@
-# src/enemy.py
-
 import pygame
-from pygame.rect import Rect
+from base_entity import BaseEntity
 
-class Enemy:
-    def __init__(self, x, y, width, height, speed):
-        self.rect = Rect(x, y, width, height)
-        self.speed = speed
-        self.vel_x = 0
-        self.vel_y = 0
-        self.on_ground = False
-        self.gravity = 0.5
-        self.max_speed = 3
+class Enemy(BaseEntity):
+    def __init__(self, x, y):
+        super().__init__(x, y, 40, 60, 2)
+        self.vel_x = self.speed  # Enemies move automatically
 
-    def apply_gravity(self):
-        self.vel_y += self.gravity
-        if self.vel_y > 10:  # Terminal vel
-            self.vel_y = 10
-
-    def move(self):
-        # Horizontal movement is constrained by vel
-        self.rect.x += self.vel_x
-        self.rect.y += self.vel_y
-
-    def check_collisions(self, platforms):
-        self.on_ground = False
-        self.rect.y += self.vel_y  # Move vertically first
-
+    def update(self, platforms):
+        super().update(platforms)
+        # Reverse direction when hitting a wall
         for platform in platforms:
-                if self.rect.colliderect(platform.rect):
-                    if self.vel_y > 0:  # Falling down
-                        self.rect.bottom = platform.rect.top
-                        self.vel_y = 0
-                        self.on_ground = True
-                    elif self.vel_y < 0:  # Jumping up
-                        self.rect.top = platform.rect.bottom
-                        self.vel_y = 0
+            if self.rect.colliderect(platform.rect):
+                self.vel_x *= -1
 
-        self.rect.x += self.vel_x  # Move horizontally
-        for platform in platforms:
-                if self.rect.colliderect(platform.rect):
-                    if self.vel_x > 0:  # Moving right
-                        self.rect.right = platform.rect.left
-                    elif self.vel_x < 0:  # Moving left
-                        self.rect.left = platform.rect.right
-
-
-
-    def update(self, platforms, player):
-        self.apply_gravity()
-        self.move()
-        self.check_collisions(platforms)
-
-    def follow_player(self, player):
-        pass
-
-class Ninja(Enemy):  # Inherit from the Enemy class
-    def __init__(self, x, y, width, height, speed):
-        super().__init__(x, y, width, height, speed)
-        self.image = pygame.Surface((width, height))
-        self.image.fill((0, 255, 0))  
-        self.rect = self.image.get_rect(topleft=(x, y))
-
-    def update(self, platforms, player):
-        self.apply_gravity()
-        self.follow_player(player)
-        self.move()
-        self.check_collisions(platforms)
+class Ninja(Enemy):
+    def __init__(self, x, y):
+        super().__init__(x, y)
+        self.image = pygame.Surface((40, 60))
+        self.image.fill((0, 255, 0))  # Green ninja
 
     def follow_player(self, player):
         if self.rect.centerx < player.rect.centerx:
@@ -74,7 +25,11 @@ class Ninja(Enemy):  # Inherit from the Enemy class
         elif self.rect.centerx > player.rect.centerx:
             self.vel_x = -self.speed  # Move left if ninja is to the right of the player
         else:
-            self.vel_x = 0  # Stop moving if ninja is aligned with the player
+            self.vel_x = 0  # Stop moving if aligned
+
+    def update(self, platforms, player):
+        self.follow_player(player)
+        super().update(platforms)
 
     def draw(self, screen):
         screen.blit(self.image, self.rect)
